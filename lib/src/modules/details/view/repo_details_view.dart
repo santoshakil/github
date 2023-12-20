9399ce08a73bc20184665026dde7d90b2b2d8770
Santo Shakil <shakil.mezbah@gmail.com>
2023-12-20 22:34:41 +0600

    WIP: RepositoryDetailsView

---
+++ lib/src/modules/details/view/repo_details_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RepositoryDetailsView extends StatelessWidget {
+  static const route = 'details';
+
+  const RepositoryDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
